1089164a8451217a5543d51869811afbf662e845
Alexandra Marques <prof.alexandra.marques@gmail.com>
2025-06-23 17:12:19 -0300

    Add files via upload

--- contabancaria/Conta.java
+++ contabancaria/Conta.java
@@ -1,9 +1,10 @@
 package contabancaria;
-
 /**
  *
  * @author Alexandra
  */
+import java.util.ArrayList;
+
 public class Conta {
     //Atributos
     private int agencia;
@@ -11,17 +12,20 @@ public class Conta {
     private String CPF;
     private double saldo;    
     private double limite;
-    //1 -`Poupança, 
+    //1 - Poupança, 
     //2 - Conta Corrente
     //3 - Conta Salário    
-    private int tipoConta;
-    
+    private int tipoConta;    
+    private ArrayList<String> movimentacoes = 
+            new ArrayList<String>();
+ 
     //Construtor
     public Conta(int agencia, int numero){
       this.agencia = agencia;
       this.numero = numero;
       this.saldo  = 0;
       this.limite = 0;
+      movimentacoes.add("Conta aberta!");
     }
     //GET /SET
 
@@ -43,7 +47,7 @@ public class Conta {
      * @return the numero
      */
     public int getNumero() {
-        return numero;
+        return this.numero;
     }
 
     /**
@@ -107,6 +111,33 @@ public class Conta {
      */
     public void setTipoConta(int tipoConta) {
         this.tipoConta = tipoConta;
+    }
+    
+    //Depositar
+    public double depositar(double valor){
+        movimentacoes.add("Depósito de "+valor);
+        return this.saldo = this.saldo + valor; 
+       
+    }
+    //Saque da conta
+    public double sacar(double valor){
+      if ((this.saldo - valor) >=0){
+        this.saldo = this.saldo - valor;  
+        movimentacoes.add("Saque de "+valor);
+      }
+      else{
+           System.out.println("Saldo insuficiente!");
+      }
+      return saldo;
+    }
+    
+    //Extrato
+    public void gerarExtrato(){
+        System.out.println("=== Extrato ===");
+        for (int i=0;i<movimentacoes.size();i++){
+           System.out.println(movimentacoes.get(i)); 
+        }
+        System.out.println("Saldo Atual: "+this.saldo);
     }
     
 }
--- contabancaria/ContaBancaria.java
+++ contabancaria/ContaBancaria.java
@@ -1,22 +1,112 @@
 package contabancaria;
-
-import javax.swing.*;
 /**
  *
  * @author Alexandra
  */
-public class ContaBancaria {
+import javax.swing.*;
+import java.util.ArrayList; 
+import java.util.List;    
 
+public class ContaBancaria {
+    //Lista de Contas Global
+    public static List<Conta> listaConta = new ArrayList<>();
+    
+    //Abrir Conta
+    public static Conta abrirConta(){
+        int agencia = Integer.parseInt(JOptionPane.
+                showInputDialog("Digite o número da agencia"));
+        int numero = Integer.parseInt(JOptionPane.
+                showInputDialog("Digite o número da conta"));
+        Conta conta = new Conta(agencia, numero);
+        return conta;
+    }
+    
+    //Listar todas as contas
+    public static void listarContas(){
+        for (int i = 0;i<listaConta.size();i++){
+            System.out.println(listaConta.get(i).getNumero());
+        }
+    }
+    
+    //Método para pesquisar na Lista de Contas
+    public static int pesquisarLista(int numero){
+       int i = 0;
+       boolean achou = false;
+       while(!achou && i<listaConta.size()){           
+           System.out.println(i);
+           achou = (numero==listaConta.get(i).getNumero());
+           i++;    
+       }
+       if(!achou) {
+           i = -1;
+       }
+       return i;
+    }
+    
+    public static void depositar(int numero){
+       int i = pesquisarLista(numero);
+       if(i>-1){
+           Conta conta = listaConta.get(i);
+           double valor = Double.parseDouble(
+                 JOptionPane.showInputDialog("Digite o valor: "));
+           conta.depositar(valor);
+           JOptionPane.showMessageDialog(null,
+                   "Saldo: "+conta.getSaldo());
+       }else{
+           JOptionPane.showMessageDialog(null, 
+                   "Conta inexistente!");
+       }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //Conta Bancária
-        //A - Abrir Conta
-        //D - Depositar
-        //R - Sacar 
-        //V - Saldo
-        //S - Sair
+        //1 - Abrir Conta
+        //2 - Depositar
+        //3 - Sacar 
+        //4 - Saldo
+        //9 - Sair
+        
+        String menu = "1 - Abrir Conta"+
+                      "\n2 - Depositar"+
+                      "\n3 - Sacar"+
+                      "\n4 - Ver Saldo"+
+                      "\n5 - Gerar Extrato "+
+                      "\n6 - Listar todas as contas"+
+                      "\n9 - Sair";
+        String op = "0";       
+
+        while(!op.equals("9")){
+            op = JOptionPane.showInputDialog(null,menu);
+            switch (op) {
+                case "1":
+                    listaConta.add(abrirConta());
+                    break;
+                case "2":
+                    int numero = Integer.parseInt(
+                            JOptionPane.showInputDialog(null,"Digite o número da conta"));
+                    depositar(numero);
+                    break;
+                case "3":
+                    //sacar
+                    break;
+                case "4":
+                    //verSaldo;
+                    break;
+                case "5":
+                    //gerarExtrato;
+                    break;
+                case "6":
+                    listarContas();
+                    break;
+                case "9":
+                    JOptionPane.showMessageDialog(null, "Volte Sempre!");
+                    break;
+                    
+            }
+        }
         
     }
     
