c7868d2527e9f320595f23339765feebe2dd10d7
Alexandra Marques <prof.alexandra.marques@gmail.com>
2025-07-14 17:06:58 -0300

    Add files via upload

---
+++ turismo/Turismo.java
@@ -0,0 +1,18 @@
+
+package turismo;
+
+/**
+ *
+ * @author User
+ */
+public class Turismo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Sistema de Turismo de Caxias do Sul");
+    }
+    
+}
